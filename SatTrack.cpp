2204083b8857d05ed4119beb80af6a60d6ab8ede
Ben Wilk <54871186+benw1916@users.noreply.github.com>
2019-10-24 11:44:59 -0400

    Added Satellite Category Chooser
    Added the satellite category chooser.  Apparently I was not pulling all the satellites that are above you at any moment.  
    
    I would implement an "All Satellites" item, but no one is going to go through 3000 satellites.

--- SatTrack.cpp
+++ SatTrack.cpp
@@ -27,13 +27,14 @@ class CurrentLocation {
 	string longitude;
 	string latitude;
 	string elevation;
-	bool UoM;
+	int satelliteCategory;
 
 public:
 	CurrentLocation() {
 		setLongitude(); 
 		setLatitude();
 		setElevation();
+		displaySatelliteCategories();
 		/*this->longitude = "-83.045753"; // This is for testing purposes; I get sick of typing them.
 		this->latitude = "42.331429";
 		this->elevation = "200";*/
@@ -51,14 +52,20 @@ protected:
 	}
 
 	void setElevation() {
-		cout << "Enter your elevation: ";
+		cout << "Enter your elevation from sea level (in meters): ";
 		cin >> this->elevation;
 	}
 
-	void setUnitOfMeasure() {
-		cout << "Imperial or Metric: ";
-		cin >> this->UoM;
+	void displaySatelliteCategories() {
+		int satelliteChoice;
+		for (int i = 0; i < satelliteCategories.size(); i++) {
+			cout << "\n" << i + 1 << ". " << satelliteCategories.at(i);
+		}
+		cout << "\nYour choice: ";
+		cin >> satelliteChoice;
+		this->satelliteCategory = satelliteValues.at(satelliteChoice);
 	}
+
 
 public:
 	string getLongitude() {
@@ -73,9 +80,14 @@ public:
 		return this->elevation;
 	}
 
+	int getCategory() {
+		return this->satelliteCategory;
+	}
+
 private:
-
-
+	vector<string> satelliteCategories = { "Amateur radio", "Beidou Navigation System", "Brightest", "Celestis", "CubeSats", "Disaster monitoring", "Earth resources", "Education", "Engineering" "Experimental", "Flock", "Galileo", "Geodetic", "Geostationary", "Global Positioning System(GPS) Constellation", "Global Positioning System(GPS) Operational", "Globalstar", "Glonass Constellation", "Glonass Operational", "GOES", "Gonets", "Gorizont", "Intelsat", "Iridium", "IRNSS", "ISS", "Lemur", "Military", "Molniya", "Navy Navigation Satellite System", "NOAA", "O3B Networks", "Orbcomm", "Parus", "QZSS", "Radar Calibration", "Raduga", "Russian LEO Navigation", "Satellite - Based Augmentation System", "Search & rescue", "Space & Earth Science", "Starlink", "Strela", "Tracking and Data Relay Satellite System", "Tselina", "Tsikada", "Tsiklon", "TV", "Weather", "Westford Needles", "XM and Sirius", "Yaogan" };
+	vector<int> satelliteValues = { 18, 35, 1, 45, 32, 8, 6, 29, 28, 19, 48, 22, 27, 10, 50, 20, 17, 51, 21, 5, 40, 12, 11, 15, 46, 2, 49, 30, 14, 24, 4, 43, 16, 38, 47, 31, 13, 25, 23, 7, 26, 52, 39, 9, 44, 42, 41, 34, 3, 37, 33, 36 };
+	
 };
 
 class SatelliteInformation {
@@ -283,8 +295,6 @@ public:
 	string getAltitude() {
 		return this->altitude;
 	}
-
-
 };
 
 class GetWebData {
@@ -331,11 +341,7 @@ public:
 
 private:
 	string downloadSatelliteList(CurrentLocation udcl) {
-		return cpr::Get(cpr::Url{ getAPIURL(udcl.getLongitude(), udcl.getLatitude(), udcl.getElevation()) }).text;
-	}
-
-	string downloadSatelliteList() {
-		return cpr::Get(cpr::Url{ getAPIURL("-83.045753", "42.331429", "656") }).text;
+		return cpr::Get(cpr::Url{ getSatellitesAboveURL(udcl) }).text;
 	}
 
 	vector<string> convertSatelliteToJSON(string rawSatelliteOutput) {
@@ -359,8 +365,16 @@ private:
 		}
 	}
 
-	string getAPIURL(string longitude, string latitude, string elevation) {
-		return "https://www.n2yo.com/rest/v1/satellite/above/" + longitude + "/" + latitude + "/" + elevation + "/70/18/&apiKey=DARUHH-CVU8AH-H9U5KE-47PU";
+	string getAPIKey() {
+		return "DARUHH-CVU8AH-H9U5KE-47PU";
+	}
+
+	string getSatellitesAboveURL(CurrentLocation cl) {
+		return "https://www.n2yo.com/rest/v1/satellite/above/" + cl.getLatitude() + "/" + cl.getLongitude() + "/" + cl.getElevation() + "/90/" + to_string(cl.getCategory()) + "/&apiKey=" + getAPIKey();
+	}
+
+	string getSpecificSatelliteDataURL(string satelliteChoice, CurrentLocation cl) {
+		return "http://www.n2yo.com/rest/v1/satellite/visualpasses/" + satelliteChoice + "/" + cl.getLatitude() + "/" + cl.getLongitude() + "/" + cl.getElevation() + "/0/300/&apiKey=" + getAPIKey();
 	}
 
 };
